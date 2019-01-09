234cb4be6436cd035b98b36fbb33358d3f30d417
bcavenagh <cavenagh.benj@gmail.com>
2019-01-09 16:27:22 -0500

    mobile event cards

--- src/Components/UI/Events/Event/Event.js
+++ src/Components/UI/Events/Event/Event.js
@@ -23,10 +23,10 @@ class Event extends Component {
     const user = fire.auth().currentUser.uid;
     participantsRef.once('value', snap => {
       snap.forEach((participant) => {
-        console.log(participant);
+        // console.log(participant);
         if(participant.key === user){
           this.setState({userRSVP: participant.val()});
-          console.log("Participant: " + participant + "valeu: " +  participant.val());
+          // console.log("Participant: " + participant + "valeu: " +  participant.val());
         }
         switch(participant.val()){
           case 'Going':
@@ -77,10 +77,11 @@ class Event extends Component {
   }
   render(){
     return(
-    
+    <>
     <div className={classes.row}>
       <div className={classes.card}>
-        <div className={classNames(
+        <div className={
+          classNames(
           classes.wrapper,
             {
               [classes.Birthday]: this.props.event.eventType === 'birthday',
@@ -139,6 +140,47 @@ class Event extends Component {
             </ul>
           </div>
         </div>
+      </div>
+    </div>
+
+    <div className={classes.mobilerow}>
+      <div className={classes.card}>
+        <div className={
+          classNames(
+          classes.wrapper,
+            {
+              [classes.Birthday]: this.props.event.eventType === 'birthday',
+              [classes.Entertainment]: this.props.event.eventType === 'entertainment',
+              [classes.FoodDrink]: this.props.event.eventType === 'fooddrink',
+              [classes.GameNight]: this.props.event.eventType === 'gamenight',
+              [classes.Meetup]: this.props.event.eventType === 'meetup',
+              [classes.Outdoors]: this.props.event.eventType === 'outdoors',
+              [classes.Party]: this.props.event.eventType === 'party',
+              [classes.Travel]: this.props.event.eventType === 'travel',
+              
+            })}>
+          <div className={classNames(
+            classes.date, classes.Default,
+              // {
+              //   [classes.Salmon]: this.props.event.colorHex === '#FF5733', 
+              //   [classes.Mint]: this.props.event.colorHex === '#07E796',
+              //   [classes.Brass]: this.props.event.colorHex === '#E79007', 
+              //   [classes.Royal]: this.props.event.colorHex === '#7117C7',
+              // }
+          )}>
+            <span className={classes.day}>{this.props.day}</span> 
+            <span className={classes.month}>{this.props.month}</span>
+            <span className={classes.year}>{this.props.year}</span>
+          </div>
+          <div className={classes.mobiledata}>
+            <div className={classes.content}>
+              {/* <span className={classes.author}>Jane Doe</span> */}
+              <h1 className={classes.title} >{this.props.name}</h1>
+              <label className={classes.menubutton} onClick={(e) => this.props.open(this.props.event)}><span></span></label>
+            </div>
+            {/* <input type="checkbox" id={classes.showmenu} /> */}
+          </div>
+        </div>
         <div className={classes.MobileRSVP}>
           <ul className={classes.mobilemenucontent}>
             <li>
@@ -166,7 +208,12 @@ class Event extends Component {
         </div>
       </div>
     </div>
+</>
     )}
   }
 
 export default withStyles(classes)(Event);
+
+
+
+

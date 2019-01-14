dc47ed71fa559310cdb6a1eec6977d203e4532c6
bcavenagh <cavenagh.benj@gmail.com>
2019-01-13 22:29:53 -0500

    added bouncing arrows to dash placeholder

--- src/Components/UI/DashPlaceholder/DashPlaceholder.js
+++ src/Components/UI/DashPlaceholder/DashPlaceholder.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from './DashPlaceholder.module.scss';
 import classNames from 'classnames';
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
 
 const dashPlaceholder = (props) => (
   <div className={classes.Dash}>
+    <ChevronLeft className={classes.Chevron1}/>
+    <ChevronLeft className={classes.Chevron2}/>
     <div className={classes.TextContainer}>
-      <p className={classes.Text}>Please select a group to get started!</p>
-      </div>
+      <p className={classes.Text}>Select or create a group to get started!</p>
+    </div>
   </div>
 );
 
